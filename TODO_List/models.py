18d9922fbe1731f769bb48dcd5b7f80ed1dc25e0
Tanner Kvarfordt <tanner.kvarfordt@aggiemail.usu.edu>
2017-04-21 18:16:59 +0000

    Began work on TODO_List models

--- TODO_List/models.py
+++ TODO_List/models.py
@@ -3,3 +3,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class TODO_Item(models.Model):
+    description = models.CharField('Description', max_length=200)
+    title = models.CharField('Title', max_length=50)
+    due_date = models.DateField('Due_Date')
+    # TODO: add priority
