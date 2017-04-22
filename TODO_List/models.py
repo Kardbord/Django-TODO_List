1c251b5e2fc69067108295c0e91bb2570e765b1a
Tanner Kvarfordt <tanner.kvarfordt@aggiemail.usu.edu>
2017-04-22 22:45:03 +0000

    Initialized the datase (see django tutorial part 2)

--- TODO_List/models.py
+++ TODO_List/models.py
@@ -4,6 +4,12 @@ from django.db import models
 
 # Create your models here.
 
+PRIORITIES = (
+    ('HIG', 'high'),
+    ('MED', 'medium'),
+    ('LOW', 'low'),
+)
+
 class TODO_Item(models.Model):
     description = models.TextField('Description', max_length=300)
     title = models.CharField('Title', max_length=50)
@@ -11,8 +17,5 @@ class TODO_Item(models.Model):
     isComplete = models.BooleanField('is completed', default=False)
     priority = models.CharField(max_length=3, choices=PRIORITIES)
     
-PRIORITIES = (
-    ('HIG', 'high'),
-    ('MED', 'medium'),
-    ('LOW', 'low'),
-)
+    def __str__(self):
+        return self.title
