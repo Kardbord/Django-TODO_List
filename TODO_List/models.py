14b481b827b38b89cf375408afe0b96a0d64951a
Tanner Kvarfordt <tanner.kvarfordt@aggiemail.usu.edu>
2017-04-22 21:54:59 +0000

    Wrote model for TODO_Item

--- TODO_List/models.py
+++ TODO_List/models.py
@@ -5,7 +5,14 @@ from django.db import models
 # Create your models here.
 
 class TODO_Item(models.Model):
-    description = models.CharField('Description', max_length=200)
+    description = models.TextField('Description', max_length=300)
     title = models.CharField('Title', max_length=50)
-    due_date = models.DateField('Due Date')
-    # TODO: add priority
+    due_date = models.DateTimeField('Due Date')
+    isComplete = models.BooleanField('is completed', default=False)
+    priority = models.CharField(max_length=3, choices=PRIORITIES)
+    
+PRIORITIES = (
+    ('HIG', 'high'),
+    ('MED', 'medium'),
+    ('LOW', 'low'),
+)
