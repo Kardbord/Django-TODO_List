05d183260f3dffab97f49932aed5b98b793c0382
Tanner Kvarfordt <tanner.kvarfordt@aggiemail.usu.edu>
2017-04-23 00:07:08 +0000

    renamed 'home' page to 'index' accessed via .../todo

--- TODO_List/urls.py
+++ TODO_List/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
+    url(r'^$', views.index, name='index'),
     url(r'^new/$', views.new, name='new'),
     url(r'^(?P<todo_id>[0-9]+)/$', views.detail, name='detail'),
 ]
