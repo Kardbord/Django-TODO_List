da20bc39009b8ba2355607f1c6ff63c6c92d06ef
Tanner Kvarfordt <tanner.kvarfordt@aggiemail.usu.edu>
2017-04-23 00:22:08 +0000

    Tied index view to some html

--- TODO_List/views.py
+++ TODO_List/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import TODO_Item
+
 # Create your views here.
 
 # TODO: create an edit view with pattern like .../home/[pk]/edit if time
 
 # home will need options to add, delete, or mark TODO items as complete
-def home(request):
-    return HttpResponse("Hello world! You're at the TODO homepage.")
+def index(request):
+    todo_list = TODO_Item.objects.order_by('-due_date')
+    context = {
+        'todo_list': todo_list,
+    }
+    return render(request, 'TODO_List/index.html', context)
     
 def detail(request, todo_id):
     return HttpResponse("You are looking at todo item number %s." % todo_id)
