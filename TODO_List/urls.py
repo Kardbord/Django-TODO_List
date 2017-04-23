def422dc2251e79684cb50017a89ae9558ce6856
Tanner Kvarfordt <tanner.kvarfordt@aggiemail.usu.edu>
2017-04-23 00:48:47 +0000

    namespaced urls

--- TODO_List/urls.py
+++ TODO_List/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'TODO_List'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^new/$', views.new, name='new'),
