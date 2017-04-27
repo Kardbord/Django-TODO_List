6d985167756e1dc8b1708e68b01789858e2b5f37
Tanner Kvarfordt <tanner.kvarfordt@aggiemail.usu.edu>
2017-04-27 01:57:38 +0000

    Added a slider and a button to ask user when they want to be nagged

--- TODO_List/static/TODO_List/script.js
+++ TODO_List/static/TODO_List/script.js
@@ -1,7 +1,7 @@
 function nag(nagThreshold) {
     var todos = document.getElementsByTagName('li');
     
-    if (todos.length >= nagThreshold) {
+    if (todos.length > nagThreshold) {
         var div = document.createElement('div');
         var p = document.createElement('p');
         p.textContent = "Looks like you have a lot to do! Get cracking!";
@@ -12,6 +12,13 @@ function nag(nagThreshold) {
     }
 }
 
+function updateButton(node) {
+    var forms = document.getElementsByClassName('nagoptions');
+    var button = forms[0].getElementsByTagName('button');
+    var slider = forms[0].getElementsByTagName('input');
+    button[0].textContent = "Nag if more than " + slider[0].value + " TODOs are present.";
+}
+
 window.onload = function() {
-    nag(3);
+    nag(5);
 }
